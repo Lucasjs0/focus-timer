f9bd1ddb2cb65cd6a48ad848dbc845a7d52506b3
Lucas Janaudis Soares <108514144+Lucasjs0@users.noreply.github.com>
2023-01-11 23:00:55 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,61 +1,31 @@
-import resetControls from "./controls.js"
-import { Timer } from "./timer.js"
+import Controls  from "./controls.js"
+import Timer  from "./timer.js"
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  minutesDisplay,
+  secondsDisplay 
+} from "./elements.js"
 
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
-let minutes = Number(minutesDisplay.textContent)
-let timerTimeOut
+const controls = Controls({
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet
+})
 
 const timer = Timer({
   minutesDisplay,
   secondsDisplay,
-  timerTimeOut,
-  resetControls,
+  resetControls: controls.reset
 })
 
+const sound = Sound()
 
-buttonPlay.addEventListener('click', function() {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')
+Events({controls, timer, sound})
 
-  timer.countDown()
-})
 
-buttonPause.addEventListener('click', function() {
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
-  clearTimeout(timerTimeOut)
-})
-
-buttonStop.addEventListener('click', function() {
-  resetControls()
-  timer.resetTimer()
-})
-
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide')
-  buttonSoundOff.classList.remove('hide')
-})
-
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOff.classList.add('hide')
-  buttonSoundOn.classList.remove('hide')
-})
-
-buttonSet.addEventListener('click', function() {
-  let newMinutes = prompt('Quantos minutos?')
-  if(!newMinutes) {
-    timer.resetTimer()
-    return
-  }
-  minutes = newMinutes
-  updateTimerDisplay(minutes, 0)
-})
--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,22 @@
-export function Timer({
+import Sounds from "./sounds.js"
+export default function Timer({
   minutesDisplay,
   secondsDisplay,
-  timerTimeOut,
   resetControls
 }) {
 
-  function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  let timerTimeOut
+  let minutes = Number(minutesDisplay.textContent)
+
+  function updateDisplay(newMinutes, seconds) {
+    newMinutes = newMinutes === undefined ? minutes : newMinutes
+    seconds = seconds === undefined ? 0 : seconds
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
     secondsDisplay.textContent = String(seconds).padStart(2, "0")
   }
 
-  function resetTimer() {
-    updateTimerDisplay(minutes, 0)
+  function reset() {
+    updateDisplay(minutes, 0)
     clearTimeout(timerTimeOut)
   }
 
@@ -19,11 +24,14 @@ export function Timer({
     timerTimeOut = setTimeout(function() {
       let seconds = Number(secondsDisplay.textContent)
       let minutes = Number(minutesDisplay.textContent)
+      let countDownEnd = minutes <= 0 && seconds <= 0
 
-      updateTimerDisplay(minutes, 0)
+      updateDisplay(minutes, 0)
 
-      if(minutes <= 0) {
+      if(countDownEnd) {
         resetControls()
+        updateDisplay()
+        Sounds().timeEnd()
         return
       }
 
@@ -32,16 +40,24 @@ export function Timer({
         --minutes
       }
 
-      updateTimerDisplay(minutes, String(seconds - 1))
-
-
-
+      updateDisplay(minutes, String(seconds - 1))
       countDown()
     }, 1000)
   }
 
+  function updateMinutes(newMinutes) {
+    minutes = newMinutes
+  }
+
+  function hold() {
+    clearTimeout(timerTimeOut)
+  }
+
   return {
     countDown,
-    resetTimer
+    reset,
+    updateDisplay,
+    updateMinutes,
+    hold
   }
 }
